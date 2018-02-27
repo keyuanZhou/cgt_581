5f973cb26fc1d479686ea3a310c7c758a7aa63e0
keyuanZhou <yuan9442@gmail.com>
2018-02-26 19:24:27 -0500

    20180226
    initiation

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CGT 581 Project</title>
+  <meta charset="utf-8">
+  <link href="./css/chord.css" rel="stylesheet">
+</head>
+<body>
+  <svg width="960" height="960"></svg>
+  <script src="https://d3js.org/d3.v4.min.js"></script>
+  <script src="./js/chord.js"></script>
+</body>
+</html>
